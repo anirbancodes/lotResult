868c45a9c3378a53c1927aa2b8f1ac08bb4f11e5
Anirban Deb <anirbandeb430@gmail.com>
2023-09-28 03:36:15 +0530

    j

--- time.js
+++ time.js
@@ -18,7 +18,7 @@ let date, time, hms;
 
 async function fetchTime() {
   let apiData;
-  await fetch("https://qctime1.p.rapidapi.com/time", optionsQcTime1)
+  await fetch("https://time.api.mylot.org")
     .then((res) => res.json())
     .then((res) => {
       apiData = res;
@@ -26,7 +26,7 @@ async function fetchTime() {
       // date = res.date;
     })
     .catch(async (err) => {
-      await fetch("https://livetime.p.rapidapi.com/time", optionsLiveTime)
+      await fetch("https://time.api.mylotto.in")
         .then((res) => res.json())
         .then((res) => {
           apiData = res;
